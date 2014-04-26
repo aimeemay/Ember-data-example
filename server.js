3f02e201a026cc4dd08cc94a04124603c062b774
Aimee Leong <aimeemayleong@gmail.com>
2014-04-26 13:49:38 +0800

    Add Coffee WORKING

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-
-
-
- // Look at the ember: http://jsbin.com/izijal/9/edit
+// Look at the ember: http://jsbin.com/izijal/9/edit
 var express = require('express');
 var livereload = require('express-livereload');
 var fs = require('fs');
 var app = express();
-
 
 livereload(app, config={})
 
@@ -19,7 +15,6 @@ app.configure(function() {
   app.use(app.router);
   app.use(express.static(__dirname + '/static'));
 });
-
 
 app.get('/api/v1/coffees', function (req, res) {
   fs.readFile('coffeeData.js', 'utf8', function (err, data) {
@@ -38,49 +33,63 @@ app.get('/api/v1/coffees', function (req, res) {
     price: x.price
    };
   })
-  console.log(coffee_list);
+  // console.log(coffee_list);
   
   //res.writeHead(200, { 'Content-Type': 'application/json' });
   return res.json({'coffees': coffee_list});
   });
-
   
 });
 
+//POST of a new coffee
 app.post('/api/v1/coffees', function (req, res) {
-  //1. create new coffee object
-  var newCoffee = req.body.coffee
-  // console.log(newCoffee);
-  //console.log(coffee.length);
+  //1. Get new coffee data
+  var newCoffeeData = req.body.coffee
 
-  var newCoffeeStr = {
-    id : coffee.length,
-    name : newCoffee.name,
-    short_description : newCoffee.short_description,
-    long_description : newCoffee.long_description,
-    price : newCoffee.price,
+  //2. Get master coffee object (from file)
+  var coffeeData = fs.readFileSync('coffeeData.js', 'utf8', function (err, data) {
+    if (err) throw err;
+  });
+  var coffeeObj = JSON.parse(coffeeData);
+
+  //3. Build new coffee object
+  var newCoffeeObj = {
+    id : coffeeObj.length,
+    name : newCoffeeData.name,
+    short_description : newCoffeeData.short_description,
+    long_description : newCoffeeData.long_description,
+    price : newCoffeeData.price,
     image : '',
-    who_drinks_it: newCoffee.who_drinks_it,
-    how_to_drink: newCoffee.how_to_drink,
+    who_drinks_it: newCoffeeData.who_drinks_it,
+    how_to_drink: newCoffeeData.how_to_drink,
     gallery: []
   }
-  // console.log(newCoffeeStr);
-  coffee.push(newCoffeeStr);
-  console.log(coffee);
+    // console.log(newCoffeeObj);
 
-  //2. append to coffee array
+  //4. Add new coffee obj to master coffee obj
+  coffeeObj.push(newCoffeeObj);
+  console.log(coffeeObj);
+
+  //5. Save updated coffee obj to file
+  var coffeeObjStr = JSON.stringify(coffeeObj);
+
+  fs.writeFile('coffeeData.js', coffeeObjStr, function (err) {
+    if (err) throw err;
+    console.log('It\'s saved!');
+  });
 
 });
 
 app.get("/api/v1/coffees/:id", function(req, res){
- // var coffees = fs.readFile('coffeeData,js', function (err, data) {
- //  if (err) throw err;
- //  console.log(data);
- // });
- res.json({coffees: coffee.filter(function(a, i) {
-   return +req.params.id === +a.id;
- })})
-})
+ fs.readFile('coffeeData.js', 'utf8', function (err, data) {
+    if (err) throw err;
+    var json = JSON.parse(data);
+
+    res.json({coffees: json.filter(function(a, i) {
+      return +req.params.id === +a.id;
+    })});
+ });
+});
 
 
 app.listen(4242, function() {
